ab244ffa83ce4a09f0bf441a47ba824056c0c34c
Michael Savitski <tug28287@temple.edu>
2019-10-24 16:58:40 -0400

    Modified training and functionality for bounding box regression.

--- src/main.py
+++ src/main.py
@@ -1,7 +1,7 @@
 import sys
 import os
 from os import path
-import tensorflow as tf
+from tensorflow import keras
 from trainConvnet import TrainConvnet
 from freezeModel import FreezeModel
 from keras import layers, models, utils
@@ -13,17 +13,16 @@ def main():
 
     args = parse_args(sys.argv)
     __training_path = args[0]
-    __test_path = args[1]
-    __initialize = args[2]
-    __freeze = args[3]
+    __initialize = args[1]
+    __freeze = args[2]
     __image_width = 150
     __image_height = 150
     __model_path = path.abspath(path.dirname(path.dirname(__file__)) + "/model")
     __model_file = "/model.h5"
-    __model = initialize_convent(__model_path, __model_file, __initialize, __image_height, __image_width)
+    __model = initialize_convent(__model_path, __model_file, __initialize, __image_height, __image_width, __training_path)
 
-    if not __training_path == "" and not __test_path == "":
-        __model = train_convnet(__training_path, __test_path, __model, __image_width, __image_height)
+    if not __training_path == "":
+        __model = train_convnet(__training_path, __model, __image_width, __image_height)
         __model.save(__model_path + "saved_model.h5")
 
     if __freeze:
@@ -51,27 +50,18 @@ def parse_args(argv):
                 args[0] = path.abspath(arg)
                 print("Training path is " + str(args[0]))
 
-        if last_arg == "-test":
-
-            if not path.exists(arg):
-                print("Path " + arg + " does not exist. Terminating.")
-                exit()
-            else:
-                args[1] = path.abspath(arg)
-                print("Test path is " + str(args[1]))
-
         if arg == "-init":
             i = input("User is requesting to initialize model. Are you sure? [Type \'Yes\']: ")
 
             if i == "Yes":
                 print("Model will be initialized.")
-                args[2] = True
+                args[1] = True
             else:
                 print("Model will not be initialized. Terminating.")
                 exit()
 
         if arg == "-freeze":
-            args[3] = True
+            args[2] = True
 
         if arg == "-help":
             print("BinBot Training software use:")
@@ -83,26 +73,18 @@ def parse_args(argv):
 
         last_arg = arg
 
-    if not (path.exists(args[0]) or not path.exists(args[1])) and not args[3]:
+    if not (path.exists(args[0])) and not args[3]:
         print("Not enough valid path arguments found. Terminating.")
         exit()
 
     return args
 
 
-def initialize_convent(model_path, model_file, initialize, image_height, image_width):
+def initialize_convent(model_path, model_file, initialize, image_height, image_width, training_path):
     """This method will load the existing convnet, or initialize it if none exists
 
     model_path: the expected file path to the existing model"""
     """
-    __model = models.Sequential()
-    __model.add(layers.Conv2D(32, (3, 3), activation='relu', input_shape=(150, 150, 3)))
-    __model.add(layers.MaxPooling2D((2, 2)))
-    __model.add(layers.Conv2D(64, (3, 3), activation='relu'))
-    __model.add(layers.MaxPooling2D((2, 2)))
-    __model.add(layers.Conv2D(64, (3, 3), activation='relu'))
-    """
-
     __model = models.Sequential([
         layers.Conv2D(16, 3, padding='same', activation='relu', input_shape=(image_height, image_width, 3)),
         layers.MaxPooling2D(),
@@ -115,6 +97,59 @@ def initialize_convent(model_path, model_file, initialize, image_height, image_w
         layers.Flatten(),
         layers.Dense(512, activation='relu'),
         layers.Dense(1, activation='sigmoid')
+    ])
+    """
+
+    training_list = os.listdir(training_path)
+    classes = len(training_list)
+    input_shape = (image_height, image_width, 3)
+
+    __model = models.Sequential([
+        layers.Conv2D(16, kernel_size=(3, 3), padding='same', strides=1, input_shape=input_shape),
+        layers.LeakyReLU(alpha=0.2),
+        layers.Conv2D(16, kernel_size=(3, 3), padding='same', strides=1),
+        layers.LeakyReLU(alpha=0.2),
+        layers.MaxPooling2D(pool_size=(2, 2)),
+
+        layers.Conv2D(32, kernel_size=(3, 3), padding='same', strides=1),
+        layers.LeakyReLU(alpha=0.2),
+        layers.Conv2D(32, kernel_size=(3, 3), padding='same', strides=1),
+        layers.LeakyReLU(alpha=0.2),
+        layers.MaxPooling2D(pool_size=(2, 2)),
+
+        layers.Conv2D(64, kernel_size=(3, 3), padding='same', strides=1),
+        layers.LeakyReLU(alpha=0.2),
+        layers.Conv2D(64, kernel_size=(3, 3), padding='same', strides=1),
+        layers.LeakyReLU(alpha=0.2),
+        layers.MaxPooling2D(pool_size=(2, 2)),
+
+        layers.Conv2D(128, kernel_size=(3, 3), padding='same', strides=1),
+        layers.LeakyReLU(alpha=0.2),
+        layers.Conv2D(128, kernel_size=(3, 3), padding='same', strides=1),
+        layers.LeakyReLU(alpha=0.2),
+        layers.MaxPooling2D(pool_size=(2, 2)),
+
+        layers.Conv2D(256, kernel_size=(3, 3), padding='same', strides=1),
+        layers.LeakyReLU(alpha=0.2),
+        layers.Conv2D(256, kernel_size=(3, 3), padding='same', strides=1),
+        layers.LeakyReLU(alpha=0.2),
+        layers.MaxPooling2D(pool_size=(2, 2)),
+
+        layers.Flatten(),
+
+        layers.Dense(1240),
+        layers.LeakyReLU(alpha=0.2),
+        layers.Dense(640),
+        layers.LeakyReLU(alpha=0.2),
+        layers.Dense(480),
+        layers.LeakyReLU(alpha=0.2),
+        layers.Dense(120),
+        layers.LeakyReLU(alpha=0.2),
+        layers.Dense(62),
+        layers.LeakyReLU(alpha=0.2),
+
+        layers.Dense(classes + 4),
+        layers.LeakyReLU(alpha=0.2)
     ])
 
     if not path.exists(model_path):
@@ -130,22 +165,22 @@ def initialize_convent(model_path, model_file, initialize, image_height, image_w
     return __model
 
 
-def train_convnet(training_path, test_path, model, image_width, image_height):
+def train_convnet(training_path, model, image_width, image_height):
     """This method will begin the training of the convnet by calling the TrainConvnet class
 
     training_path: file path to the set of training images
     test_path: file path to the set of test images
     model: the convnet data model"""
-    __training = TrainConvnet(training_path, test_path, model, image_width, image_height)
+    __training = TrainConvnet(training_path, model, image_width, image_height)
     __model = __training.start()
     return __model
 
 
-def export_convnet(model_path, model_file, graph_file):
+def export_convnet(model_path, model_file):
     """This method will export the model to a file by calling the FreezeModel class
 
     model: the convnet data model"""
-    __freeze = FreezeModel(model_path, model_file, graph_file)
+    __freeze = FreezeModel(model_path, model_file)
     __freeze.start()
     return __freeze.success
 
--- src/trainConvnet.py
+++ src/trainConvnet.py
@@ -1,52 +1,52 @@
 from __future__ import absolute_import, division, print_function, unicode_literals
 import tensorflow as tf
 import os
-from os import path
-from tensorflow import keras
-from keras import datasets, layers, models
-from keras.preprocessing import image
-from keras.preprocessing.image import ImageDataGenerator
+import glob
 import matplotlib.pyplot as plt
 import numpy as np
-from keras.optimizers import SGD
+import xmltodict
+from tensorflow import keras
+from PIL import Image, ImageDraw
+from keras.preprocessing.image import ImageDataGenerator
+from keras.optimizers import Adam
+from keras.backend import maximum, minimum
+from sklearn.preprocessing import LabelBinarizer
+from sklearn.model_selection import train_test_split
 
 
 class TrainConvnet:
-    def __init__(self, training_path, test_path, model, image_width, image_height):
+    def __init__(self, training_path, model, image_width, image_height):
         self.__training_path = training_path
-        self.__test_path = test_path
         self.__training_total = 0
         self.__test_total = 0
-        self.__training_classes = 0
-        self.__test_classes = 0
+        self.__training_classes = []
+        self.__test_classes = []
+        self.__training_boxes = []
+        self.__test_boxes = []
         self.__model = model
-        self.__training_gen = ImageDataGenerator()
-        self.__test_gen = ImageDataGenerator()
+        self.__training_images = ImageDataGenerator()
+        self.__test_images = ImageDataGenerator()
         self.__image_width = image_width
         self.__image_height = image_height
 
     def start(self):
         """This method will be called externally by main to begin training the model"""
         training_list = os.listdir(self.__training_path)
-        test_list = os.listdir(self.__test_path)
         self.__training_classes = len(training_list)
-        self.__test_classes = len(os.listdir(self.__test_path))
-        training_classes = []
-        test_classes = []
 
-        for directory in training_list:
-            training_classes.append(directory)
-            self.__training_total += len(os.listdir(path.abspath(self.__training_path + '/' + directory)))
+        self.__training_images = self.__load_images(self.__training_path)
+        self.__training_boxes, self.__training_classes = self.__load_bounding_boxes(self.__training_path)
+        print("Training total = " + str(len(self.__training_images)))
 
-        for directory in test_list:
-            test_classes.append(directory)
-            self.__test_total += len(os.listdir(path.abspath(self.__test_path + '/' + directory)))
+        boxes = np.array(self.__training_boxes)
+        encoder = LabelBinarizer()
+        classes_onehot = encoder.fit_transform(self.__training_classes)
 
-        print("Training total, test total = " + str(self.__training_total) + ', ' + str(self.__test_total))
+        Y = np.concatenate([boxes, classes_onehot], axis=1)
+        X = np.array(self.__training_images)
+        x_train, x_test, y_train, y_test = train_test_split(X, Y, test_size=0.1)
 
-        self.__training_gen = self.__load_images(self.__training_path)
-        self.__test_gen = self.__load_images(self.__test_path)
-        history = self.__train_model()
+        history = self.__train_model(x_train, x_test, y_train, y_test)
         self.__display_results(history)
 
         return self.__model
@@ -55,57 +55,103 @@ class TrainConvnet:
         """This method will load the images from the designated directory for training / testing
 
         path: File path to the relevant set of images being loaded"""
-        generator = ImageDataGenerator(rescale=1./255)
-        data_gen = generator.flow_from_directory(batch_size=128,
-                                                 directory=image_path,
-                                                 shuffle=True,
-                                                 target_size=(self.__image_height, self.__image_width),
-                                                 class_mode='binary')
-        return data_gen
+        classes = os.listdir(image_path)
+        images = []
 
-    def __train_model(self):
+        for class_name in classes:
+            image_paths = glob.glob(image_path + "/" + class_name + '/*.jpg')
+
+            for image_file in image_paths:
+                image = Image.open(image_file).resize((self.__image_width, self.__image_height))
+                image = np.asarray(image)
+                images.append(image)
+
+        return images
+
+    def __load_bounding_boxes(self, image_path):
+        """This method will load the bounding box data from the designated directory for training / testing
+
+        path: File path to the relevant set of xml files being loaded"""
+        classes = os.listdir(image_path)
+        bounding_boxes = []
+        classes_raw = []
+
+        for class_name in classes:
+            annotation_paths = glob.glob(image_path + "/" + class_name + '/*.xml')
+
+            for xml_file in annotation_paths:
+                x = xmltodict.parse(open(xml_file, 'rb'))
+                bounding_box = x['annotation']['object']['bndbox']
+                bounding_box = np.array([int(bounding_box['xmin']), int(bounding_box['ymin']), int(bounding_box['xmax']),
+                                         int(bounding_box['ymax'])])
+                bounding_box2 = [None] * 4
+                bounding_box2[0] = bounding_box[0]
+                bounding_box2[1] = bounding_box[1]
+                bounding_box2[2] = bounding_box[2]
+                bounding_box2[3] = bounding_box[3]
+                bounding_box2 = np.array(bounding_box2) / self.__image_width
+                bounding_boxes.append(bounding_box2)
+                classes_raw.append(x['annotation']['object']['name'])
+
+        return bounding_boxes, classes_raw
+
+    def __train_model(self, x_train, x_test, y_train, y_test):
         """This method will operate the loop that trains the convnet on the loaded images
 
         model: the convnet data model"""
-        """
-        model.compile(optimizer='adam',
-                      loss='binary_crossentropy',
-                      metrics=['accuracy'])
-        """
 
-        opt = SGD(lr=0.01)
-        self.__model.compile(opt, loss='binary_crossentropy', metrics=['accuracy'])
+        self.__model.compile(
+            optimizer=Adam(lr=0.0001),
+            loss=self._calculate_loss,
+            metrics=[self.__calculate_iou]
+        )
 
         self.__model.summary()
 
-        history = self.__model.fit_generator(
-            self.__training_gen,
-            steps_per_epoch=self.__training_total,
-            epochs=15,
-            validation_data=self.__test_gen,
-            validation_steps=self.__test_total
-
-
+        history = self.__model.fit(
+            x_train,
+            y_train,
+            validation_data=(x_test, y_test),
+            epochs=100,
+            batch_size=3
         )
+
         return history
+
+    def __calculate_iou(self, target_boxes, predicted_boxes):
+        xa = maximum(target_boxes[..., 0], predicted_boxes[..., 0])
+        ya = maximum(target_boxes[..., 1], predicted_boxes[..., 1])
+        xb = minimum(target_boxes[..., 2], predicted_boxes[..., 2])
+        yb = minimum(target_boxes[..., 3], predicted_boxes[..., 3])
+        inter_area = maximum(0.0, xb - xa) * maximum(0.0, yb - ya)
+        box_a_area = (target_boxes[..., 2] - target_boxes[..., 0]) * (target_boxes[..., 3] - target_boxes[..., 1])
+        box_b_area = (predicted_boxes[..., 2] - predicted_boxes[..., 0]) * (predicted_boxes[..., 3] - predicted_boxes[..., 1])
+        iou = inter_area / (box_a_area + box_b_area - inter_area)
+        return iou
+
+    def _calculate_loss(self, y_true, y_predicted):
+        mse = tf.losses.mean_squared_error(y_true, y_predicted)
+        iou = self.__calculate_iou(y_true, y_predicted)
+        return mse + (1-iou)
 
     def __display_results(self, history):
         """This method will output the results of the training to the command prompt, such as the success and
         loss between training and testing the model"""
-        accuracy = history.history['accuracy']
-        value_accuracy = history.history['val_accuracy']
+        # accuracy = history.history['accuracy']
+        # value_accuracy = history.history['val_accuracy']
         loss = history.history['loss']
         value_loss = history.history['val_loss']
-        epochs_range = range(15)
+        epochs_range = range(100)
 
         plt.figure(figsize=(8, 8))
+        """
         plt.subplot(1, 2, 1)
         plt.plot(epochs_range, accuracy, label='Training Accuracy')
         plt.plot(epochs_range, value_accuracy, label='Validation Accuracy')
         plt.legend(loc='lower right')
         plt.title('Training and Validation Accuracy')
-
         plt.subplot(1, 2, 2)
+        """
         plt.plot(epochs_range, loss, label='Training Loss')
         plt.plot(epochs_range, value_loss, label='Validation Loss')
         plt.legend('upper right')
